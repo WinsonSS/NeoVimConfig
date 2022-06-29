9ec3a36131c473f9058c7f7361660668d632ba34
YoSora <2251189307@qq.com>
2022-06-30 01:01:51 +0800

    add autopair plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,25 +4,30 @@
 --vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- nightfox 主题
-  	use "EdenEast/nightfox.nvim"
-	-- 下方状态栏
-	use
-	{
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
-	-- 文件管理工具
-	use
-	{
-		"Shougo/defx.nvim",
-		requires =
-		{
-			"kristijanhusak/defx-icons", -- dexf文件管理图标支持
-			"t9md/vim-choosewin", -- 窗口选择器，可以让defx使用i打开文件
-		}
-	}
+    -- nightfox 主题
+    use "EdenEast/nightfox.nvim"
+    -- 下方状态栏
+    use
+    {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    -- 文件管理工具
+    use
+    {
+        "Shougo/defx.nvim",
+        requires =
+        {
+            "kristijanhusak/defx-icons", -- dexf文件管理图标支持
+            "t9md/vim-choosewin", -- 窗口选择器，可以让defx使用i打开文件
+        }
+    }
+    use
+    {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 end)
